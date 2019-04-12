f3fb27b4bf7218a43addbab80dd3c11868e70886
Vitalijs Glods <vitalijs.glods@transactpro.lv>
2019-04-12 11:39:46 +0300

    GW3-3690 + fix warning

--- src/main/java/com/transactpro/gateway/operation/interfaces/request/base/MoneyInterface.java
+++ src/main/java/com/transactpro/gateway/operation/interfaces/request/base/MoneyInterface.java
@@ -5,6 +5,7 @@ import com.transactpro.gateway.operation.interfaces.OperationInterface;
 
 public interface MoneyInterface<T> extends OperationInterface {
 
+    @SuppressWarnings("unchecked")
     default T setMoney(Money money) {
         getOperation().getRequest().getData().setMoney(money);
         return (T) getOperation();
